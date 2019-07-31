010b366e19069a84ee49b793d21f280c19c69329
muheez <muheezakanni@gmail.com>
2019-07-31 14:28:23 +0100

    add Views structure, add view1 content

--- App.js
+++ App.js
@@ -5,21 +5,75 @@ import {
   View,
   Text,
   StatusBar,
+  Animated,
+  Dimensions
 } from 'react-native';
+import TimeAndDate from './components/TimeAndDate';
+
+const screen = Dimensions.get("screen");
 
 const App = () => {
   return (
     <Fragment>
       <StatusBar barStyle="light-content" backgroundColor="black" />
-      <SafeAreaView style={{ backgroundColor: "black" }}>
-        <Text style={{ color: "white" }}>Hello there</Text>
-      </SafeAreaView>
+      <View style={{ flex: 1, backgroundColor: "black" }}>
+        <SafeAreaView style={{ backgroundColor: "black" }}>
+          <Animated.Text style={styles.headerText}>Good morning Charles</Animated.Text>
+        </SafeAreaView>
+        <View style={{ flex: 1 }}>
+          <Animated.View style={styles.view3}>
+
+          </Animated.View>
+          <Animated.View style={styles.view2}>
+
+          </Animated.View>
+          <Animated.View style={styles.view1}>
+            <TimeAndDate />
+          </Animated.View>
+        </View>
+      </View>
+      
     </Fragment>
   );
 };
 
 const styles = StyleSheet.create({
-  
+  headerText: { 
+    color: "white", 
+    fontSize: 38, 
+    lineHeight: 45, 
+    paddingHorizontal: 47, 
+    paddingVertical: 40,
+    fontFamily: "MaisonNeue-Medium",
+    letterSpacing: -0.41
+  },
+  view1: {
+    position: "absolute",
+    width: screen.width,
+    height: screen.height,
+    backgroundColor: "#3D21E8",
+    borderTopRightRadius: 31,
+    left: -40,
+    top: 120
+  },
+  view2: {
+    position: "absolute",
+    width: screen.width,
+    height: screen.height,
+    backgroundColor: "#2164E8",
+    borderTopRightRadius: 40,
+    left: -30,
+    top: 60
+  },
+  view3: {
+    position: "absolute",
+    width: screen.width,
+    height: screen.height,
+    backgroundColor: "#33E1FF",
+    borderTopRightRadius: 46,
+    left: -20,
+    top: 0
+  }
 });
 
 export default App;
---
+++ components/TimeAndDate.js
@@ -0,0 +1,61 @@
+import React, {Component} from "react";
+import {
+    View,
+    Text,
+    StyleSheet,
+    Image,
+    Animated
+} from "react-native";
+
+export default class TimeAndDate extends Component {
+    render() {
+        return(
+            <View style={styles.container}>
+                <View style={{ marginBottom: 59 }}>
+                   <Text style={{ color: "white", fontSize: 38, fontFamily: "MaisonNeue-Medium", lineHeight: 52, letterSpacing: 1.22 }}>
+                       09:26
+                    </Text>
+                    <Text style={{ color: "white", fontSize: 22, lineHeight: 30, fontFamily: "MaisonNeue-Light", opacity: .8,letterSpacing: 0.71 }}>
+                        Wednesday 6th Feb
+                    </Text>
+                </View>
+                <View style={{ marginBottom: 59 }}>
+                    <View style={{ flexDirection: "row", alignItems: "center", marginBottom: 10 }}>
+                        <Image style={{ width: 28, height: 28, marginRight: 16 }} 
+                            source={require("../assets/icons/sunny.png")} 
+                            resizeMethod="auto" 
+                            resizeMode="contain" />
+                        <Text style={{ fontSize: 26, color: "white", letterSpacing: 1.22, fontFamily: "MaisonNeue-Medium", lineHeight: 36 }}>
+                            32°
+                        </Text>
+                    </View>
+                    <Text style={{ color: "white", fontSize: 22, lineHeight: 30, fontFamily: "MaisonNeue-Light", opacity: .8,letterSpacing: 0.71 }}>
+                        It's going to be sunny all day long
+                    </Text>
+                </View>
+                <View>
+                    <View style={{ flexDirection: "row", alignItems: "center" }}>
+                        <Image style={{ width: 28, height: 28, marginRight: 16 }}
+                            source={require("../assets/icons/sunset.png")}
+                            resizeMethod="auto"
+                            resizeMode="contain" />
+                        <Text style={{ fontSize: 26, color: "white", letterSpacing: 1.22, fontFamily: "MaisonNeue-Medium", lineHeight: 36 }}>
+                            18:45
+                        </Text>
+                    </View>
+                    <Text style={{ color: "white", fontSize: 22, lineHeight: 30, fontFamily: "MaisonNeue-Light", opacity: .8,letterSpacing: 0.71 }}>
+                        Sunset will be clear
+                    </Text>
+                </View>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        position: "relative",
+        paddingHorizontal: 78,
+        paddingTop: 49
+    }
+})
